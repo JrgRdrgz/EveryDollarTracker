214981a333c30c40005ecfbe05ac878112e11b2e
Jorge Rodriguez <jorerodriguez98@gmail.com>
2021-07-19 20:25:31 -0500

    got registration page working.

--- app/src/main/java/com/example/everydollartracker/register.java
+++ app/src/main/java/com/example/everydollartracker/register.java
@@ -1,17 +1,25 @@
 package com.example.everydollartracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
+import org.jetbrains.annotations.NotNull;
 import org.w3c.dom.Text;
 
 public class register extends AppCompatActivity {
@@ -46,14 +54,67 @@ public class register extends AppCompatActivity {
 
     }
 
-    private void registeruser() {
+    private void registeruser(){
         String email=editTextemail.getText().toString().trim();
         String name=editTextname.getText().toString().trim();
         String password=editTextpassword.getText().toString().trim();
 
         if(name.isEmpty()){
-
+            editTextname.setError("This field is required");
+            editTextname.requestFocus();
+            return;
         }
 
+        if(email.isEmpty()){
+            editTextemail.setError("This field is required");
+            editTextemail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            editTextpassword.setError("This field is required");
+            editTextpassword.requestFocus();
+            return;
+        }
+
+        if(password.length() < 6){
+            editTextpassword.setError("Password must be at least 7 characters long");
+            editTextemail.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextemail.setError("Invalid Email Address");
+            editTextemail.requestFocus();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NotNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+                            User user = new User(name, email);
+
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>(){
+                                @Override
+                                public void onComplete(@NotNull Task<Void> task) {
+
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(register.this, "Account Created", Toast.LENGTH_LONG).show();
+
+                                    }else{
+                                        Toast.makeText(register.this,"Failed to create account", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+                        }else{
+                            Toast.makeText(register.this,"Failed to create account", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 }
