763b1ee8f8a8d6c59bbd63d78be1bc12f1c90b21
Jorge Rodriguez <jorerodriguez98@gmail.com>
2021-07-20 15:15:54 -0500

    added bottom navigation page

--- app/src/main/java/com/example/everydollartracker/homepage.java
+++ app/src/main/java/com/example/everydollartracker/homepage.java
@@ -1,8 +1,13 @@
 package com.example.everydollartracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class homepage extends AppCompatActivity {
 
@@ -10,5 +15,9 @@ public class homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this,  R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
