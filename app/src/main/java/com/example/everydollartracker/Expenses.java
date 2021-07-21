f00bf78a1c6ae427d2f208fd664c4ef43373e71e
kmn2463 <70443810+kmn2463@users.noreply.github.com>
2021-07-21 11:45:40 -0500

    Income, Expenses

---
+++ app/src/main/java/com/example/everydollartracker/Expenses.java
@@ -0,0 +1,14 @@
+package com.example.everydollartracker;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Expenses extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_income);
+    }
+}
