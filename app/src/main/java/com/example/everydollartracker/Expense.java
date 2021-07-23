562c3299a1e7ea2af60881e47c77c46c234dc024
kmn2463 <70443810+kmn2463@users.noreply.github.com>
2021-07-23 17:34:15 -0500

    Add the Realtime Database SDK

--- app/src/main/java/com/example/everydollartracker/Expense.java
+++ app/src/main/java/com/example/everydollartracker/Expense.java
@@ -3,6 +3,8 @@ package com.example.everydollartracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Expense extends AppCompatActivity {
 
@@ -10,5 +12,6 @@ public class Expense extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expense);
+
     }
 }
--- app/src/main/java/com/example/everydollartracker/Income.java
+++ app/src/main/java/com/example/everydollartracker/Income.java
@@ -3,12 +3,18 @@ package com.example.everydollartracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Income extends AppCompatActivity {
-
+    EditText editTextAmountIn,editTextDateIn,editTextNoteIn;
+    Button buttonCancelIn,buttonSaveIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_income);
+        editTextAmountIn=findViewById(R.id.editTextAmountIn);
+        editTextDateIn=findViewById(R.id.editTextDateIn);
+        editTextNoteIn=findViewById(R.id.editTextNoteIn);
     }
 }
