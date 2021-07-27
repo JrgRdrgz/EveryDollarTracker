2d2d55b04a9947aad50e135f1d3053203131f7b3
Ragheedda <ragheedda@gmail.com>
2021-07-27 01:58:36 -0500

    New

--- app/src/main/java/com/example/everydollartracker/Settings.java
+++ app/src/main/java/com/example/everydollartracker/Settings.java
@@ -59,6 +59,6 @@ public class Settings extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        return inflater.inflate(R.layout.fragment_profile_page, container, false);
     }
 }
