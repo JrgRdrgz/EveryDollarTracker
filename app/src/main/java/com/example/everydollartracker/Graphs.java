581ea18c30bd97a15a39e12331ca7b13fbdf4b9e
Ragheedda <ragheedda@gmail.com>
2021-07-28 08:30:21 -0500

    New

--- app/src/main/java/com/example/everydollartracker/Graphs.java
+++ app/src/main/java/com/example/everydollartracker/Graphs.java
@@ -1,17 +1,14 @@
 package com.example.everydollartracker;
 
-import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.graphics.Color;
-import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
 
 import org.eazegraph.lib.charts.PieChart;
 import org.eazegraph.lib.models.PieModel;
--- app/src/main/java/com/example/everydollartracker/Profile_Page.java
+++ app/src/main/java/com/example/everydollartracker/Profile_Page.java
@@ -1,50 +1,83 @@
 package com.example.everydollartracker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 
-public class Profile_Page extends AppCompatActivity
-{
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Profile_Page#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Profile_Page extends Fragment {
 
-    private ImageView profile_image;
-    private Uri imageUri;
-    private FirebaseStorage storage;
-    private StorageReference storageReference;
-
-    //EditText Email, Password, DOF, FullName, UserName, Income, Expenses;
     DatabaseReference DBR;
-    //Button BUpdate;
-    Intent myIntent = new Intent(this, Dashboard.class);
+
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public Profile_Page() {
+
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Dashboard.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Profile_Page newInstance(String param1, String param2) {
+        Profile_Page fragment = new Profile_Page();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile_page);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
 
-
-        final ImageView profile_image = (ImageView) findViewById(R.id.profile_image);
-        final EditText Email = (EditText) findViewById(R.id.email_id);
-        final EditText Password = (EditText) findViewById(R.id.password_id);
-        final EditText DOF = (EditText) findViewById(R.id.dateofbirth_id);
-        final EditText FullName = (EditText) findViewById(R.id.Fullname);
-        final EditText UserName = (EditText) findViewById(R.id.Username);
-        final EditText Income = (EditText) findViewById(R.id.Income_M);
-        final EditText Expenses = (EditText) findViewById(R.id.Expense_M);
-        Button BUpdate = (Button) findViewById(R.id.upp);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_profile_page, container, false);
+        final ImageView profile_image = (ImageView) view.findViewById(R.id.profile_image);
+        final EditText Email = (EditText) view.findViewById(R.id.email_id);
+        final EditText Password = (EditText) view.findViewById(R.id.password_id);
+        final EditText DOF = (EditText) view.findViewById(R.id.dateofbirth_id);
+        final EditText FullName = (EditText) view.findViewById(R.id.Fullname);
+        final EditText UserName = (EditText) view.findViewById(R.id.Username);
+        final EditText Income = (EditText) view.findViewById(R.id.Income_M);
+        final EditText Expenses = (EditText) view.findViewById(R.id.Expense_M);
+        Button BUpdate = (Button) view.findViewById(R.id.upp);
         UUser user1 = new UUser();
 
         String emailVal = Email.getText().toString().trim();
@@ -108,14 +141,13 @@ public class Profile_Page extends AppCompatActivity
             profile_info pro = new profile_info(Email.getText().toString().trim(), Password.getText().toString().trim(), DOF.getText().toString(), FullName.getText().toString().trim(), UserName.getText().toString().trim(), Income.getText().toString().trim(), Expenses.getText().toString().trim());
             user1.add(pro).addOnSuccessListener(suc ->
             {
-                Toast.makeText(this, "Data Updated", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(this, "Data Updated", Toast.LENGTH_SHORT).show();
             }).addOnFailureListener(er ->
             {
-                Toast.makeText(this, "" + er.getMessage(), Toast.LENGTH_SHORT).show();
+                //Toast.makeText(this, "" + er.getMessage(), Toast.LENGTH_SHORT).show();
             });
         });
+        return view;
+
     }
-
-
-                //Toast.makeText(Dashboard.this, "Profile Updated!" , Toast.LENGTH_SHORT).show();
 }
