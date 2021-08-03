48a9fdc8cf2ae0c0d298eee9634cfbed49d5610c
kmn2463 <70443810+kmn2463@users.noreply.github.com>
2021-08-03 15:56:56 -0500

    Revert "fix save and load data to firebase"
    This reverts commit 251042ec

---
+++ app/src/main/java/com/example/everydollartracker/UserFireStore.java
@@ -0,0 +1,29 @@
+package com.example.everydollartracker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserFireStore {
+    public List<InExStore> inExStoreArrayList;
+
+    /*public UserFireStore(){
+
+    }
+
+    public List<InExStore> getInExStores(){
+        return inExStoreArrayList;
+
+    }
+    public void addInOrEx(double amount,String type,String date,String source,String note) {
+        InExStore newInEx = new InExStore(amount,type,date,source,note);
+        inExStoreArrayList.add(newInEx);
+    //public  ArrayList<InExStore> inExStoreArrayList;
+    //public String userID;
+
+
+    //public void addInOrEx(double amount,String type,String date,String source,String note) {
+    //    InExStore newInEx = new InExStore(amount,type,date,source,note);
+    //    inExStoreArrayList.add(newInEx);
+   // }
+}*/
+}
