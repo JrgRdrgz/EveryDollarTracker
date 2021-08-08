2dc5b03367caa67592ce602db130df67d8f0d08a
Ragheedda <ragheedda@gmail.com>
2021-08-07 19:01:46 -0500

    New

--- app/src/main/java/com/example/everydollartracker/edit_profile.java
+++ app/src/main/java/com/example/everydollartracker/edit_profile.java
@@ -1,12 +1,20 @@
 package com.example.everydollartracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class edit_profile extends AppCompatActivity {
 
@@ -54,6 +62,26 @@ public class edit_profile extends AppCompatActivity {
                     updateemail.requestFocus();
                     return;
                 }
+
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                        .setDisplayName(FullNameVal)
+                        .build();
+
+                user.updateProfile(profileUpdates)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Log.d(TAG, "User profile updated.");
+                                }
+                            }
+                        });
+
+                Intent intent = new Intent(getApplicationContext(), Settings.class);
+                startActivity(intent);
+
 
             }
         });
